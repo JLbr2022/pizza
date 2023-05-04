5a7199e06333863351711589aa098a046f82c6af
José Freites <jlfu2004@gmail.com>
2023-05-03 23:57:44 -0300

    Rename comp Avatar to Header

---
+++ frontEnd/src/components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import DrawerMenu from './DrawerMenu'
+
+export default function Avatar() {
+  const buildings = {
+    name: 'BuildingsBackground',
+    image: '/src/public/images/BuildingsBackground.svg',
+    height: '8vh',
+    width: '100%'
+  }
+
+  const avatar = {
+    name: 'Pizzatown Delivery',
+    image: '/src/public/images/avatar.svg',
+    height: '4rem',
+    width: '3rem'
+  }
+
+  return (
+    <header
+      className="
+        bg-contain bg-center bg-repeat-x 
+        flex justify-between items-center
+      "
+      style={{
+        backgroundImage: `url(${buildings.image})`,
+        height: buildings.height,
+        width: buildings.width
+      }}
+    >
+      <div
+        className="
+        flex justify-center
+        bg-center bg-no-repeat bg-contain
+        ml-2 mt-4
+        "
+        style={{
+          backgroundImage: `url(${avatar.image})`,
+          height: avatar.height,
+          width: avatar.width
+        }}
+      />
+      <div className="flex justify-between">
+        <h1 className="text-yellow-500 font-black text-2xl">
+          PizzaTown Delivery
+        </h1>
+      </div>
+
+      <DrawerMenu />
+    </header>
+  )
+}
