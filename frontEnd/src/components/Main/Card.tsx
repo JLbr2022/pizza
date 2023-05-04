277f127d1f41c6978119c34a7fb43f4fd20dc85e
José Freites <jlfu2004@gmail.com>
2023-05-03 23:57:01 -0300

    Card mobile size done

--- frontEnd/src/components/Main/Card.tsx
+++ frontEnd/src/components/Main/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Promos from './Promos'
+import Promos from '../Footer/Promos'
 
 function Card() {
   const pzImage = {
@@ -8,28 +8,28 @@ function Card() {
   return (
     <div className="max-w-sm rounded  shadow-lg">
       <img className="w-full" src={pzImage.image} alt="Pizza con tomate" />
-      <div className="px-6 py-2">
-        <div className="font-bold text-yellow-500 text-xl mb-2 flex justify-center">
+      <div className="px-2 py-2">
+        <div className="font-bold text-yellow-500 text-xl mb-1 flex justify-center">
           Pizza con Tomate
         </div>
-        <p className="text-gray-300 text-base">
+        <p className="text-gray-300 text-base flex justify-center">
           Ingredientes: Tomate, mozzarela, Aceitunas
         </p>
-        <p className="text-yellow-500 font-semibold text-base flex">
+        <p className="text-yellow-500 font-semibold text-base flex justify-center">
           Precio: R$40
         </p>
       </div>
 
-      <div className="px-6 pt-4 pb-2">
+      <div className="pt-1">
         <button
           type="submit"
           className="
-          bg-green-900 
-          rounded-md px-3 py-1 text-sm 
+          bg-green-900
+          rounded-md text-lg 
           font-sans text-green-200 
-          mr-2 mb-2
           flex justify-center
           w-full
+          p-4
           "
         >
           Enviar información a Whatsapp
