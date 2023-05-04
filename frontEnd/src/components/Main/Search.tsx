67e94b696765b384a31ff0da2a47cbec0f211bb8
José Freites <jlfu2004@gmail.com>
2023-05-03 23:59:27 -0300

    Created search component

--- frontEnd/src/components/Main/Search.tsx
+++ frontEnd/src/components/Main/Search.tsx
@@ -17,9 +17,9 @@ function Search() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             ></path>
           </svg>
@@ -38,16 +38,7 @@ function Search() {
           placeholder="Pizza, bebida, ingrediente..."
           required
         />
-        <button
-          type="submit"
-          className="
-            text-yellow-900
-            absolute right-2.5 bottom-2.5
-            bg-blue-700 hover:bg-blue-800 
-            focus:ring-4 focus:outline-none focus:ring-blue-300 
-            font-medium rounded-lg text-sm px-4 py-2
-            dark:bg-yellow-400 dark:hover:bg-green-700 dark:focus:ring-green-500"
-        >
+        <button type="submit" className="button">
           Buscar
         </button>
       </div>
